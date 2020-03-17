250f4af05c023d1929399c221b8b5acbfd3df053
muralireddy555 <muralireddy.v555@gmail.com>
2020-03-17 12:25:55 +0530

    Initial Commit

---
+++ PulseBRNApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PulseBRNApp
+//
+//  Created by VORUGANTUI.MURALI KRISHNA REDDY on 17/03/20.
+//  Copyright © 2020 BRN Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
